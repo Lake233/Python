9670bdc7d03d6f056190dd9d2fb2a723a6fd3ff4
Lake233 <3081192900@qq.com>
2020-08-31 19:53:55 +0800

    commit practice file

---
+++ practice/List/greetings.py
@@ -0,0 +1,9 @@
+names=["lake", "alan", "windgrin", "crazywind"]
+greeting="Hello " + names[0].title() + ", how are you?"
+print(greeting)
+greeting="Hello " + names[1].title() + ", how are you?"
+print(greeting)
+greeting="Hello " + names[2].title() + ", how are you?"
+print(greeting)
+greeting="Hello " + names[3].title() + ", how are you?"
+print(greeting)
