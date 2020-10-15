728e11a3ff775f4f7c8e10d9b1c62af278cd970f
Lake233 <3081192900@qq.com>
2020-10-15 20:36:09 +0800

    practice file

---
+++ practice/user_input_and_while_loop/pizza_ingredients.py
@@ -0,0 +1,5 @@
+ingredient = input("Please enter the ingredient that you want: ")
+while ingredient != 'quit':
+    print("OK, we will add this ingredient to you.")
+    ingredient = input("Please enter the ingredient that you want: ")
+print("OK, your order will be completed soon.")
