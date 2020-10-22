5600894107cfc19c8b31ad6ea7d18e6f02f0ee38
Lake233 <3081192900@qq.com>
2020-10-22 22:19:37 +0800

    commit practice file

---
+++ practice/function/T-shirt.py
@@ -0,0 +1,4 @@
+def make_shirt(size, word):
+    print("The size of T-shirt is " + size + " and the word is " + word + ".")
+make_shirt("L", "NIKE")
+make_shirt(size="XL", word="Addidas")
---
+++ practice/function/message.py
@@ -0,0 +1,3 @@
+def display_message():
+    print("I learnt how to use function in this section.")
+display_message()
